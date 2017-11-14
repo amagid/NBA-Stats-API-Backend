a78f19e23ed83e52f53d08ee28f8630fa9947801
Aaron Magid <ahm64@case.edu>
2017-11-13 22:40:21 -0500

    feat(Routes): Added players.getAll route.

--- src/routes/players/index.js
+++ src/routes/players/index.js
@@ -1,5 +1,5 @@
 const Players = require('./players');
 
 module.exports = function(router) {
-    router.get('/', (req, res) => res.promise('You\'re a dipshit'));
+    router.get('/', (req, res) => res.promise(Players.getAll()));
 };
--- src/routes/players/players.js
+++ src/routes/players/players.js
@@ -1,3 +1,12 @@
-module.exports = {
+const Promise = require('bluebird');
+const Python = require('../../services/python');
 
+module.exports = {
+    getAll
 };
+
+function getAll() {
+    return Python.run('playerId.py').spread(function(players) {
+        return players;
+    });
+}
