e8855a123f7d315297a803798c4d81db3f26397d
Aaron Magid <ahm64@case.edu>
2017-11-15 00:19:16 -0500

    feat(tests): Player.compare when Player 1 not found.

--- tests/testPlayersRoutes.js
+++ tests/testPlayersRoutes.js
@@ -84,4 +84,33 @@ describe('Players', function () {
             });
         });
     });
+
+    describe('compare', function () {
+        it('should return an APIError with a 404 status code and an additional parameter stating which player(s) were not found', function (done) {
+            Players.compare('Alex Abrinessssss', 'LeBron James').then(betterPlayer => {
+                throw new Error(`Promise was resolved, should have been rejected. Fulfillment value:\n${JSON.stringify(betterPlayer, null, 4)}`);
+            }).catch(err => {
+                if (err && err.status === 404 && !err.additionalData.player1Valid && err.additionalData.player2Valid) {
+                    return done();
+                }
+                throw new Error(`Improper Error Response. Received: \n${JSON.stringify(err, null, 4)}`);
+            });
+        });
+    });
+
+    describe('compare', function () {
+        it('should return the name of the superior player', function (done) {
+            Players.compare('Alex Abrines', 'LeBron James').then(betterPlayer => {
+                if (betterPlayer === 'LeBron James') {
+                    return done();
+                } else if (betterPlayer === 'Alex Abrines') {
+                    throw new Error("Incorrect Player Returned.");
+                } else {
+                    throw new Error(`Malformed Return Value. Received: \n${JSON.stringify(betterPlayer, null, 4)}`);
+                }
+            }).catch(err => {
+                throw new Error(`Error Running Function:\n${JSON.stringify(err, null, 4)}`);
+            });
+        });
+    });
 });
