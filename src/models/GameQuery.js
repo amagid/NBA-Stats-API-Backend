586e36e8bc06ea1bb599ea2478491cdb943cd8a4
Aaron Magid <ahm64@case.edu>
2017-11-20 23:12:06 -0500

    feat(Models): GameQuery model.

---
+++ src/models/GameQuery.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const db = require('../services/db');
+
+//Define model
+module.exports = const GameQuery = db.define('game_queries', {
+    id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+    },
+
+    game_id: {
+        type: Sequelize.DataTypes.INTEGER
+    },
+
+    game2_id: {
+        type: Sequelize.DataTypes.INTEGER
+    },
+
+    command: {
+        type: Sequelize.DataTypes.STRING
+    }
+});
+
+//Set associations
+const User = require('./User');
+User.hasMany(GameQuery);
