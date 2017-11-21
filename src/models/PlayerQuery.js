e3a256da71e31f5861c970b7455c44572380885b
Aaron Magid <ahm64@case.edu>
2017-11-20 23:07:22 -0500

    feat(Models): PlayerQuery model.'

---
+++ src/models/PlayerQuery.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const db = require('../services/db');
+
+//Define model
+module.exports = const PlayerQuery = db.define('player_query', {
+    id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    user_id: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+
+    player_id: {
+        type: Sequelize.DataTypes.INTEGER
+    },
+
+    player2_id: {
+        type: Sequelize.DataTypes.INTEGER
+    },
+
+    command: {
+        type: Sequelize.DataTypes.STRING
+    }
+});
+
+//Set associations
+const User = require('./User');
+User.hasMany(PlayerQuery);
