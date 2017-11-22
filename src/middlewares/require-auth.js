966115ec77a00d7aa6b9dac84c5c854c394e2900
Aaron Magid <ahm64@case.edu>
2017-11-22 06:57:19 -0500

    feat(Login): Refactored require-auth middleware to use express-jwt.

--- src/middlewares/require-auth.js
+++ src/middlewares/require-auth.js
@@ -1,7 +1,8 @@
-const jwt = require('../services/jwt');
+const jwt = require('express-jwt');
 
 module.exports = requireAuthentication;
 
+/*
 function requireAuthentication(req, res, next) {
     const token = req.headers.Authorization.split(' ')[1];
     return jwt.decode(token)
@@ -12,4 +13,9 @@ function requireAuthentication(req, res, next) {
         .catch(err => {
             res.status(err.status || 500).json(err);
         });
+}
+*/
+
+function requireAuthentication(req, res, next) {
+    jwt({ secret: publicKey })(req, res, next);
 }
