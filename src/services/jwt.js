8367064068b98f2075c93e2cbc1abfd00c7ac26e
Aaron Magid <ahm64@case.edu>
2017-11-22 05:43:51 -0500

    feat(Login): Created JWTService to generate and decode JSON Web Tokens

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -1,4 +1,32 @@
+const APIError = require('../../APIError');
+const Promise = require('bluebird');
+const jwt = require('jsonwebtoken');
+const {privateKey, publicKey} = require('../../config').getKeys();
+
+const makeJWT = Promise.promisify(jwt.sign);
+const decodeJWT = Promise.promisify(jwt.verify);
+
 module.exports = {
     generate,
-    extractUserId
+    decode
 };
+
+function generate(data) {
+    return makeJWT(data, privateKey, {algorithm: 'RS256'})
+        .then(token => {
+            console.log(token);
+        })
+        .catch(err => {
+            throw APIError(err.status || 500, err.message || 'JWT Creation Failed', err);
+        });
+}
+
+function decode(token) {
+    return decodeJWT(token, publicKey)
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            throw APIError(err.status || 500, err.message || 'JWT Invalid', err);
+        });
+}
