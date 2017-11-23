50f5d12d440854e641af6bf0632c264ef4766853
Aaron Magid <ahm64@case.edu>
2017-11-22 21:53:09 -0500

    feat(Email): Added route for verifying email.

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -8,5 +8,7 @@ function addRoutes(router) {
 
     router.post('/check-jwt', requireAuthorization, (req, res) => res.promise(Auth.checkUserData(req.user)));
 
-    router.post('/signup', (req, res) => res.promise(Auth.signup(req.body.email, req.body.password, req.body.fname, req.body.lname)))
+    router.post('/signup', (req, res) => res.promise(Auth.signup(req.body.email, req.body.password, req.body.fname, req.body.lname)));
+
+    router.get('/verify-email', (req, res) => res.promise(Auth.verifyEmail(req.query.token)));
 }
