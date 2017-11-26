ab92c65f07fd99c9395acfdb352609dd08d1075c
Aaron Magid <ahm64@case.edu>
2017-11-26 17:55:46 -0500

    feat(Login): Renamed public key file.

--- config/keys/index.js
+++ config/keys/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 function getKeys() {
     const privateKey = fs.readFileSync(path.join(__dirname, 'jwt-private.key')).toString();
-    const publicKey = fs.readFileSync(path.join(__dirname, 'jwt-public-pem.key')).toString();
+    const publicKey = fs.readFileSync(path.join(__dirname, 'jwt-public.key')).toString();
     return {
         privateKey,
         publicKey
