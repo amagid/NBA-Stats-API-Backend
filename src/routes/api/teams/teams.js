df68a7c29a08407d3aeed9d695fd6ae1a7d0b6ec
Aaron Magid <ahm64@case.edu>
2017-11-28 09:33:51 -0500

    feat(TeamRoutes): Added handler function for Teams.getAll().

--- src/routes/api/teams/teams.js
+++ src/routes/api/teams/teams.js
@@ -1,3 +1,20 @@
-module.exports = {
+const Python = require('../../../services/python');
 
+module.exports = {
+    getAll,
+    get,
+    compare
 };
+
+function getAll() {
+    return Python.run('teamId.py')
+        .then(teams => {
+            if (!teams) {
+                throw APIError(404, 'No Teams Found');
+            }
+            return teams;
+        })
+        .catch(err => {
+            throw APIError(err.status || 500, err.message || 'Error retrieving Teams', err);
+        });
+}
