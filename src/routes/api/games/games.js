29f0111e4b62bc423b60fe9e9c18729121c3017a
Aaron Magid <ahm64@case.edu>
2017-11-27 22:09:59 -0500

    feat(GameRoutes): Added routes for getting a game and comparing 3 teams.

--- src/routes/api/games/games.js
+++ src/routes/api/games/games.js
@@ -1,3 +1,34 @@
+const Python = require('../../../services/python');
+
 module.exports = {
+    tripleCompare,
+    get
 
 };
+
+function get(year, team1Name, team2Name) {
+    return Python.run('game.py', ['get', team1Name, team2Name, year])
+        .then(game => {
+            if (!game) {
+                throw APIError(404, 'Game not found');
+            }
+            return game;
+        })
+        .catch(err => {
+            throw APIError(err.status || 500, err.message || 'Error retrieving Game', err);
+        });
+}
+
+function tripleCompare(params) {
+    const { baseTeamId, team1Id, team1Year, team2Id, team2Year } = params;
+    return Python.run('game.py', ['compare', baseTeamId, team1Id, team2Id, team1Year, team2Year])
+        .then(comparison => {
+            if (!comparison) {
+                throw APIError(404, 'Game Info Not Found');
+            }
+            return comparison;
+        })
+        .catch(err => {
+            throw APIError(err.status || 500, err.message || 'Error Comparing Games', err);
+        });
+}
--- src/routes/api/games/index.js
+++ src/routes/api/games/index.js
@@ -1,5 +1,9 @@
 const Games = require('./games');
 
 module.exports = function(router) {
-    router.get('/', (req, res) => res.promise('You\'re a dipshit'));
+    router.get('/:year/:team1Name/:team2Name',
+                (req, res) => res.promise(Games.get(req.params.year, req.params.team1Name, req.params.team2Name)));
+
+    router.get('/compare/:baseTeamId/:team1Id/:team1Year/:team2Id/:team2Year',
+                (req, res) => res.promise(Games.tripleCompare(req.params)));
 };
