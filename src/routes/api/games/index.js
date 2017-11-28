954735a8463fb0ef885ed02417e4f3ffd8af2a1e
Aaron Magid <ahm64@case.edu>
2017-11-27 22:12:01 -0500

    fix(GameRoutes): Reordered parameters for Game.get() route to match frontend request.

--- src/routes/api/games/index.js
+++ src/routes/api/games/index.js
@@ -1,8 +1,8 @@
 const Games = require('./games');
 
 module.exports = function(router) {
-    router.get('/:year/:team1Name/:team2Name',
-                (req, res) => res.promise(Games.get(req.params.year, req.params.team1Name, req.params.team2Name)));
+    router.get('/:team1Id/:team2Id/:year',
+                (req, res) => res.promise(Games.get(req.params.year, req.params.team1Id, req.params.team2Id)));
 
     router.get('/compare/:baseTeamId/:team1Id/:team1Year/:team2Id/:team2Year',
                 (req, res) => res.promise(Games.tripleCompare(req.params)));
