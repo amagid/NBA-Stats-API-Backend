f35f4e0ad8ae36b08d59898f2704ab377be7e1e3
Nathaniel Celeste <nathanielceleste@gmail.com>
2017-11-27 22:18:29 -0500

    added more to Game, fixed main method to work with requests according to trello

--- python/game.py
+++ python/game.py
@@ -59,8 +59,20 @@ class Game:
         for game in games:
             self.games.append(dict(zip(keys, game)))
 
+        self.stats = dict()
+        self.stats['W/L'] = self.winlose_to_number()
+        self.stats['PLUS_MINUS'] = self.plusminus_avg()
+        self.stats['FG_PCT'] = 0
+        self.stats['FG3_PCT'] = 0
+        self.stats['FT_PCT'] = 0
+        self.stats['AST'] = 0
+        self.stats['BLK'] = 0
+        self.stats['REB'] = 0
+        self.stats['STL'] = 0
+
+    # method to compare a team across two matchups
     def compare_games(self, game_two):
-        better_matchup = Game
+        better_matchup = self
 
         # check to see if we are comparing two valid teams
         if self.teams[0] != game_two.teams[0]:
@@ -78,14 +90,17 @@ class Game:
             else:
 
                 # If the team won or lost both games pick the better team based on PLUS_MINUS
-                if self.games[0]["PLUS_MINUS"] > game_two.games[0]["PLUS_MINUS"]:
+                if self.plusminus_avg() > game_two.plusminus_avg():
                     better_matchup = self
-                else:
+                elif self.plusminus_avg() < game_two.plusminus_avg():
                     better_matchup = game_two
+                else:
+                    better_matchup = self
+                    # If
 
         return self.teams[0] + " " + "performed better against the " + better_matchup.teams[1]
 
-    #calcuates the average winlose rate for a matchup of two teams
+    # calculates the average winlose rate for a matchup of two teams
     def winlose_to_number(self):
         winlose_count = 0
         total_games = len(self.games)
@@ -98,13 +113,47 @@ class Game:
 
         return winlose_count / total_games
 
+    # calculates the average plus/minus for a matchup of two teams
+    def plusminus_avg(self):
+        plusminus_total = 0
+        total_games = len(self.games)
+
+        for x in range(0, total_games):
+            plusminus_total += self.games[x]["PLUS_MINUS"]
+
+        return plusminus_total / total_games
+
+
+    def calculate_combined_shotPCT(self, game):
+        shot_PCT_total = 0
+        shot_types = 3
+
+        shot_PCT_total += self.games[game]["FG_PCT"]
+        shot_PCT_total += self.games[game]["FG3_PCT"]
+
+
+
+# for testing purposes
+pp = pprint.PrettyPrinter(indent=4)
+datgame = Game("Los Angeles Lakers", "Boston Celtics", "2016-17")
+datgame2 = Game("Los Angeles Lakers", "Minnesota Timberwolves", "2016-17")
+datgame3 = Game("Minnesota Timberwolves", "Los Angeles Lakers", "2016-17")
+datgame4 = Game("Los Angeles Lakers", "Golden State Warriors", "2016-17")
+#pp.pprint(datgame3.games)
+#pp.pprint(datgame4.games)
+#print datgame2.games
+print datgame.compare_games(datgame2)
+print datgame.compare_games(datgame4)
+#print datgame3.winlose_to_number()
+#print datgame.compare_games(datgame3)
+
 def main():
     data = read_in()
     if data[1] == 'get':
-        game = Player(data[1], data[2],"2016-17")
-        print json.dumps(player1.player_dict)
-    #if data[1] == 'compare':
-        #player1 = Player(data[2],"Base","2016-17")
-        #player2 = Player(data[3],"Base","2016-17")
+        game1 = Game(data[2],data[3],data[4])
+        print json.dumps(game1.stats)
+    if data[1] == 'compare':
+        game1 = Game(data[2],data[3],data[4])
+        game2 = Game(data[5],data[6],data[7])
 
-        print json.dumps(player1.compare_player(player2))
+        print json.dumps(game1.compare_games(game2))
