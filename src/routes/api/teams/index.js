d46ae9b35d478c032120ff925864f8614fdc947b
Aaron Magid <ahm64@case.edu>
2017-11-28 09:33:26 -0500

    feat(TeamRoutes): Added routes for Teams.getAll, .get, and .compare.

--- src/routes/api/teams/index.js
+++ src/routes/api/teams/index.js
@@ -1,5 +1,11 @@
 const Teams = require('./teams');
 
 module.exports = function(router) {
-    router.get('/', (req, res) => res.promise('You\'re a dipshit'));
+    router.get('/', (req, res) => res.promise(Teams.getAll()));
+
+    router.get('/:teamId/:dataType',
+                (req, res) => res.promise(Teams.get(req.params.teamId, req.params.dataType)));
+
+    router.get('/:team1Id/compare/:team2Id/:dataType',
+                (req, res) => res.promise(Teams.compare(req.params.team1Id, req.params.team2Id, req.params.dataType)));
 };
