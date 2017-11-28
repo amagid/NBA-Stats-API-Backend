cc7391bb2bf36a64a4413061cb71596b8566ff0b
Aaron Magid <ahm64@case.edu>
2017-11-28 09:35:09 -0500

    feat(TeamRoutes): Added handler function for Teams.get().

--- src/routes/api/teams/teams.js
+++ src/routes/api/teams/teams.js
@@ -18,3 +18,16 @@ function getAll() {
             throw APIError(err.status || 500, err.message || 'Error retrieving Teams', err);
         });
 }
+
+function get(teamId, dataType) {
+    return Python.run('Team.py', ['get', teamId, dataType])
+        .then(team => {
+            if (!team) {
+                throw APIError(404, 'Team Not Found');
+            }
+            return team;
+        })
+        .catch(err => {
+            throw APIError(err.status || 500, err.message || 'Error retrieving Team', err);
+        });
+}
