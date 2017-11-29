a7d8800bfea27e3230e8495877b639c93c352846
Aaron Magid <ahm64@case.edu>
2017-11-29 00:18:29 -0500

    feat(UserRoutes): Added basic handlers for Users.get and Users.getGameQueries.

--- src/routes/api/users/users.js
+++ src/routes/api/users/users.js
@@ -1,4 +1,8 @@
 const APIError = require('../../../APIError');
+const Users = require('../../../models/User');
+const GameQuery = require('../../../models/GameQuery');
+const TeamQuery = require('../../../models/TeamQuery');
+const PlayerQuery = require('../../../models/PlayerQuery');
 
 module.exports = {
     get,
@@ -6,3 +10,35 @@ module.exports = {
     getTeamQueries,
     getPlayerQueries
 };
+
+function get(userId) {
+    return Users.findById(userId)
+        .then(user => {
+            //Throw 404 if no user returned
+            if (!user) {
+                throw APIError(404, 'User Not Found');
+            }
+            //Only return name
+            return {
+                fname: user.dataValues.fname,
+                lname: user.dataValues.lname
+            };
+        })
+        .catch(err => {
+            throw APIError(err.status || 500, err.message || 'User Find Failed', err);
+        });
+}
+
+function getGameQueries(userId) {
+    return Users.findById(userId, {
+            include: [{
+                model: GameQuery
+            }]
+        })
+        .then(user => {
+            console.log(user);
+        })
+        .catch(err => {
+            throw APIError(err.status || 500, err.message || 'Game Query Find Failed', err);
+        });
+}
