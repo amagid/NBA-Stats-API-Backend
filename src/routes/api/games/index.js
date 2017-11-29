a76d182abdbda8fa292cd26e80ea8bf0e077dbdf
Aaron Magid <ahm64@case.edu>
2017-11-28 23:55:06 -0500

    feat(saveSearch): Added saveSearch to all relevant routes for teams and games.

--- src/routes/api/games/index.js
+++ src/routes/api/games/index.js
@@ -1,9 +1,10 @@
+const saveSearch = require('../../../middlewares/save-search');
 const Games = require('./games');
 
 module.exports = function(router) {
-    router.get('/:team1Id/:team2Id/:year',
+    router.get('/:team1Id/:team2Id/:year', saveSearch,
                 (req, res) => res.promise(Games.get(req.params.year, req.params.team1Id, req.params.team2Id)));
 
-    router.get('/compare/:baseTeamId/:team1Id/:team1Year/:team2Id/:team2Year',
+    router.get('/compare/:baseTeamId/:team1Id/:team1Year/:team2Id/:team2Year', saveSearch,
                 (req, res) => res.promise(Games.tripleCompare(req.params)));
 };
--- src/routes/api/teams/index.js
+++ src/routes/api/teams/index.js
@@ -1,11 +1,12 @@
+const saveSearch = require('../../../middlewares/save-search');
 const Teams = require('./teams');
 
 module.exports = function(router) {
     router.get('/', (req, res) => res.promise(Teams.getAll()));
 
-    router.get('/:team1Id/:dataType',
+    router.get('/:team1Id/:dataType', saveSearch,
                 (req, res) => res.promise(Teams.get(req.params.team1Id, req.params.dataType)));
 
-    router.get('/:team1Id/compare/:team2Id/:dataType',
+    router.get('/:team1Id/compare/:team2Id/:dataType', saveSearch,
                 (req, res) => res.promise(Teams.compare(req.params.team1Id, req.params.team2Id, req.params.dataType)));
 };
