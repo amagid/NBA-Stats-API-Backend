e3ccac7675a22faea54c8ae00667f26aa5e90a1e
Aaron Magid <ahm64@case.edu>
2017-11-28 23:49:01 -0500

    feat(saveSearch): Updated routing files to work with save-search middleware.

--- src/routes/api/players/index.js
+++ src/routes/api/players/index.js
@@ -1,9 +1,10 @@
+const saveSearch = require('../../../middlewares/save-search');
 const Players = require('./players');
 
 module.exports = function(router) {
     router.get('/', (req, res) => res.promise(Players.getAll()));
 
-    router.get('/:playerId/', (req, res) => res.promise(Players.get(req.params.playerId)));
+    router.get('/:player1Id/', saveSearch, (req, res) => res.promise(Players.get(req.params.player1Id)));
 
-    router.get('/:player1Id/compare/:player2Id', (req, res) => res.promise(Players.compare(req.params.player1Id, req.params.player2Id)));
+    router.get('/:player1Id/compare/:player2Id', saveSearch, (req, res) => res.promise(Players.compare(req.params.player1Id, req.params.player2Id)));
 };
--- src/routes/api/teams/index.js
+++ src/routes/api/teams/index.js
@@ -3,8 +3,8 @@ const Teams = require('./teams');
 module.exports = function(router) {
     router.get('/', (req, res) => res.promise(Teams.getAll()));
 
-    router.get('/:teamId/:dataType',
-                (req, res) => res.promise(Teams.get(req.params.teamId, req.params.dataType)));
+    router.get('/:team1Id/:dataType',
+                (req, res) => res.promise(Teams.get(req.params.team1Id, req.params.dataType)));
 
     router.get('/:team1Id/compare/:team2Id/:dataType',
                 (req, res) => res.promise(Teams.compare(req.params.team1Id, req.params.team2Id, req.params.dataType)));
